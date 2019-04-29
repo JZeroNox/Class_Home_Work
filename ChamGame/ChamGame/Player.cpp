2a6189bd4cd420c220f311acf10949ecd904b9f8
JZeroNox <40817436+JZeroNox@users.noreply.github.com>
2019-04-29 11:27:32 +0900

    Class Home Work

---
+++ ChamGame/ChamGame/Player.cpp
@@ -0,0 +1,75 @@
+#include "pch.h"
+#include "Player.h"
+
+
+
+
+Player::~Player()
+{
+}
+
+int Player::Input()
+{
+	int input;
+	cin >> input;
+	return input;
+}
+
+void Player::GameStart()
+{
+	cout << count << "번 남았습니다." << "방향을 입력하세요. 가운데 : 0 오른쪽 : 1 왼쪽 : 2" << endl;
+	int pInput = Input();
+	int gReturn = Game.Return();
+	
+	if (pInput < 0 || pInput>2)
+	{
+	while (pInput<0 || pInput>2)
+	{
+		cout << "잘못 입력했습니다. 다시 입력하세요." << endl;
+		pInput = Input();
+	}
+	}
+	
+	if (pInput == gReturn)
+	{
+		cout << "걸렸습니다."<<endl;
+		cout<<"게임 재시작 : 3" << endl;
+		cout << "게임 종료 : 4" << endl;
+		while (true)
+		{
+			pInput = Input();
+			if (pInput == 3)
+			{
+				count = 3;
+				system("cls");
+				GameStart();
+			}
+			else if (pInput == 4) { return; }
+			else
+			{
+				cout << "다시 입력하세요." << endl;
+			}
+		}
+	}
+	else
+	{
+		cout << "피했습니다." << endl;
+		count--;
+		score++;
+		cout << "당신의 점수 : " << score << endl;
+		if (count == 0) 
+		{ 
+			score = 0;
+			cout << "승리!! 다시 시작하시겠습니까? ( 예 : 3  아니오 : 4 )" << endl; 
+			while (true)
+			{
+				pInput = Input();
+				if (pInput == 3) { count = 3; system("cls"); GameStart(); }
+				else if (pInput == 4) { return; }
+				else { cout<<"다시 입력하세요."<<endl; }
+			}
+		}
+
+		GameStart();
+	}
+}
---
+++ ChamGame/ChamGame/Player.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "GameProgram.h"
+
+class Player
+{
+public:
+	Player() = default;
+	~Player();
+	int Input();
+	void GameStart();
+
+private:
+	int Record[2]{ 0, };
+	GameProgram Game;
+	int count = 3;
+	int score = 0;
+};
+
